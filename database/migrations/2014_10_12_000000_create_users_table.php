1c91c2c2ab499d4ded53b1a113b1ac2cb6a066c3
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-01-09 13:00:03 +0200

    init commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->string('name_first', 64);
+            $table->string('name_last', 64);
+            $table->string('phone', 100);
+            $table->boolean('is_active')->default(0);
+            $table->string('avatar', 100)->nullable();
+            $table->timestamp('last_visit_time')->nullable();
+            $table->string('auth_key')->nullable();
+            $table->string('access_token')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
