fbfc44cedb3ee6e43cac10e6a6be02d89e8ad0a3
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-02-01 17:33:41 +0200

    get data for charts

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -5,16 +5,43 @@ namespace App\Http\Controllers;
 use App\Models\Node;
 use App\Models\Sensor;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class StatisticController extends Controller
 {
-    public function getStatistic() : array
+    public function getStatistic(): array
     {
+        $customersCountByDates = User::customers()
+            ->get()
+            ->groupBy(function ($val) {
+                return Carbon::parse($val->created_at)->format('d.m.Y');
+            })
+            ->map(function ($customers) {
+                return collect($customers)->count();
+            })
+            ->toArray();
+
+        $nodesCountByDates = Node::get()
+            ->groupBy(function ($val) {
+                return Carbon::parse($val->created_at)->format('d.m.Y');
+            })
+            ->map(function ($customers) {
+                return collect($customers)->count();
+            })
+            ->toArray();
+
         return [
             'customers_count' => User::customers()->count(),
             'sensors_count' => Sensor::count(),
             'objects_count' => Node::count(),
+            'customersCount' => [
+                'customersLabelsByDates' => array_keys($customersCountByDates),
+                'customersDataByDates' => array_values($customersCountByDates),
+            ],
+            'nodesCount' => [
+                'nodesLabelsByDates' => array_keys($nodesCountByDates),
+                'nodesDataByDates' => array_values($nodesCountByDates),
+            ],
         ];
     }
 }
