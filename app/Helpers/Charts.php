7332d7f27833483e8b5f8e57b09ec70a8cb1000e
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-02-03 18:41:06 +0200

    charts helper done

--- app/Helpers/Charts.php
+++ app/Helpers/Charts.php
@@ -1,17 +1,28 @@
 <?php
+
 namespace App\Helpers;
 
-class Charts {
-
-    // TODO: DONE
-    public static function collectDataForMultipleData(...$args) {
-        $test = $args;
+class Charts
+{
+    public static function collectDataForMultipleData(...$args)
+    {
+        $labels = array_keys(call_user_func_array('array_merge', $args));
+        $data = [];
+        foreach ($labels as $i => $label) {
+            foreach ($args as $key => $arg) {
+                foreach ($arg as $labelArg => $value) {
+                    if ($label !== $labelArg) {
+                        $data[$key][$i] = 0;
+                    } else {
+                        $data[$key][$i] = $value;
+                        break;
+                    }
+                }
+            }
+        }
         return [
-            'labels' => [],
-            'data' => [
-                'customersDataByDates' => [],
-                'nodesDataByDates' => [],
-            ]
+            'labels' => $labels,
+            'data' => $data
         ];
     }
 }
