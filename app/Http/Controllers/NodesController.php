dd64ba91d321dea138bbfe542dc0a65c13ecf5f1
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-02-04 15:47:17 +0200

    get nodes by user

--- app/Http/Controllers/NodesController.php
+++ app/Http/Controllers/NodesController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Node;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
 
 class NodesController extends Controller
 {
-    public function getNodes() : Collection
+    public function getNodes(Request $request): Collection
     {
-        return Node::with('nodeType')->get();
+        $nodes = Node::with('nodeType');
+        if ($request->user_id) {
+            $nodes->where(['user_id' => $request->user_id]);
+        }
+        return $nodes->get();
     }
 }
