ad678257ad6ddcf502506b6efff74ca3fbc29e80
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-02-05 16:08:05 +0200

    type name attribute for sensor icon

--- app/Models/SensorIcon.php
+++ app/Models/SensorIcon.php
@@ -8,6 +8,7 @@ class SensorIcon extends Model
 {
     protected $table = 'sensor_icon';
     protected $fillable = ['sensor_type', 'icon', 'position_top', 'position_left'];
+    protected $appends = ['type_name'];
 
     public function sensor(): \Illuminate\Database\Eloquent\Relations\HasOne
     {
@@ -17,5 +18,10 @@ class SensorIcon extends Model
     public function sensorType(): \Illuminate\Database\Eloquent\Relations\HasOne
     {
         return $this->hasOne(SensorType::class, 'id', 'sensor_type');
+    }
+
+    public function getTypeNameAttribute(): string
+    {
+        return $this->sensorType()->value('name');
     }
 }
