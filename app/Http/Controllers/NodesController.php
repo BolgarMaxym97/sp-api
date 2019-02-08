379414214162a63ebc927f7a2d3fb2bf40053d30
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-02-08 16:31:50 +0200

    node remove added

--- app/Http/Controllers/NodesController.php
+++ app/Http/Controllers/NodesController.php
@@ -23,14 +23,23 @@ class NodesController extends Controller
         ];
     }
 
-    public function getTypes() : Collection
+    public function getTypes(): Collection
     {
         return NodeType::all();
     }
 
-    public function create(Request $request) : Model
+    public function create(Request $request): Model
     {
         $this->validate($request, Node::rules());
         return Node::create($request->input());
+    }
+
+    public function remove(Request $request, $id): array
+    {
+        $node = Node::find($id);
+        if (!$node) {
+            return response()->json(['messages' => ['Объект не найден']], 404);
+        }
+        return ['success' => $node->delete()];
     }
 }
