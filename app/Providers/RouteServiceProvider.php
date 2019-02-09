623e093ee56af172d75da898790ada6640525b00
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-02-09 22:06:48 +0200

    debug off

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -45,7 +45,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::middleware(['api', '_debug'])
+        Route::middleware(['api'])
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
