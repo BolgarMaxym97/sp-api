c644ddf4eb7c99b581ac4ef0e600775f583a3061
BolgarMaxym97 <bolgar.maxym@gmail.com>
2019-04-22 14:01:25 +0300

    CustomersController killed

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -17,5 +18,10 @@ class UsersController extends Controller
             'success' => $updated,
             'user' => $user
         ]);
+    }
+
+    public function getCustomers() : Collection
+    {
+        return User::customers()->with(['nodes'])->get();
     }
 }
